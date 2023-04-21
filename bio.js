6d1d8004504cb324de7713b64ab5dd501b79c1e5
jkrunscom <127169376+jkrunscom@users.noreply.github.com>
2023-04-20 21:05:31 -0500

    Add files via upload

---
+++ bio.js
@@ -0,0 +1,48 @@
+// © 2023 H8.LOL | All rights reserved.
+window.addEventListener('load', () => {
+  const bios = ["localhost ddoser", "camel#1001", "triple k on my wrist", "owner @ h8.lol", "owner @ dui.wiki", "no limit gang", "nigger hunter", "#999", "socks full of semen", "e-whore magnet"];
+  const bioContainer = document.querySelector('#bio');
+  let currentBioIndex = 0;
+
+  function typeBio(bio) {
+    let i = 0;
+    const intervalId = setInterval(() => {
+      bioContainer.innerHTML = bio.substring(0, i).split('').map(c => `<span>${c}</span>`).join('') + '<span class="cursor">┃</span>';
+      i++;
+      if (i > bio.length) {
+        clearInterval(intervalId);
+        setTimeout(() => {
+          deleteBio(bio);
+        }, 1000);
+      }
+    }, 100);
+  }
+
+  function deleteBio(bio) {
+    let i = bio.length;
+    const intervalId = setInterval(() => {
+      bioContainer.innerHTML = bio.substring(0, i).split('').map(c => `<span>${c}</span>`).join('') + '<span class="cursor">┃</span>';
+      i--;
+      if (i < 0) {
+        clearInterval(intervalId);
+        currentBioIndex++;
+        if (currentBioIndex >= bios.length) {
+          currentBioIndex = 0;
+        }
+        setTimeout(() => {
+          typeBio(bios[currentBioIndex]);
+        }, 1000);
+      }
+    }, 100);
+  }
+
+  function blinkCursor() {
+    const cursor = document.querySelector('.cursor');
+    setInterval(() => {
+      cursor.style.opacity = 1 - cursor.style.opacity;
+    }, 500);
+  }
+
+  typeBio(bios[currentBioIndex]);
+  blinkCursor();
+});
